2e8b55ce276088cdc2ef6dd3c3025857a1206b2b
Kingsley <this.onenterframe@gmail.com>
2022-03-10 10:46:23 -0700

    fix routing on netlify

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  let navigate = useNavigate();
+  function goToImport(event) {
+    event.preventDefault();
+    navigate("/import");
+  }
+
   return (
     <div className="header">
-      <button type="button" className="choice-btn">
+      <button type="button" onClick={goToImport} className="choice-btn">
         Upload your own
       </button>
     </div>
